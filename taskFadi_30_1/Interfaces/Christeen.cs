452ddbe41497cf35ae7d50d1bd90970429e3b1bd
Christeen <christeen.ramsis@gmail.com>
2021-01-31 11:59:43 +0200

    Fisrt Commit

---
+++ taskFadi_30_1/Interfaces/Christeen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taskFadi_30_1.Interfaces
+{
+    public interface Christeen
+    {
+        void ChristeenA();
+        ref int ChristeenB(ref int b);
+        void TestCommon();
+    }
+}
